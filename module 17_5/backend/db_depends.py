bf34a7435bc14d4e29e311f86b4093063cb60738
83metra <jumo_004@mail.ru>
2024-11-16 22:17:08 +0200

    Add files via upload

---
+++ module 17_5/backend/db_depends.py
@@ -0,0 +1,17 @@
+from backend.db import SessionLockal
+
+async def get_db():
+    db = SessionLockal()
+    try:
+        yield db
+    finally:
+        db.close
+        
+# async def get_db():
+#     db = SessionLocal()
+#     try:
+#         yield db
+#     finally:
+#         db.close()
+
+
